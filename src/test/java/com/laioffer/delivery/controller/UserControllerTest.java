71ea453f758ed32044352701da593a83c147c602
Yifei Zhu <yz6994@nyu.edu>
2025-02-02 10:23:02 +0800

    remove mockbean

--- src/test/java/com/laioffer/delivery/controller/UserControllerTest.java
+++ src/test/java/com/laioffer/delivery/controller/UserControllerTest.java
@@ -9,21 +9,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -47,7 +40,7 @@ public class UserControllerTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @MockBean
+    @Autowired
     private AuthenticationManager authenticationManager;
 
     private UserDto userDto;
@@ -65,7 +58,7 @@ public class UserControllerTest {
         user = new User();
         user.setId("123");
         user.setEmail(testUserEmail);
-        user.setPassword("encodedPassword");
+        user.setPassword(passwordEncoder.encode("password123"));
 
         userRepository.deleteAll();
     }
@@ -123,15 +116,6 @@ public class UserControllerTest {
     @Test
     void testLoginSuccess() throws Exception {
         userRepository.save(user);
-
-        UsernamePasswordAuthenticationToken authToken =
-            new UsernamePasswordAuthenticationToken(testUserEmail, "password123");
-        Authentication authentication =
-            new UsernamePasswordAuthenticationToken(testUserEmail, null, Collections.emptyList());
-
-        Mockito.when(authenticationManager.authenticate(Mockito.eq(authToken)))
-              .thenReturn(authentication);
-
         mockMvc.perform(post("/api/users/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
@@ -148,8 +132,7 @@ public class UserControllerTest {
 
     @Test
     void testLoginFailure() throws Exception {
-        Mockito.when(authenticationManager.authenticate(Mockito.any()))
-              .thenThrow(new BadCredentialsException("Invalid credentials"));
+        userRepository.save(user);
 
         mockMvc.perform(post("/api/users/login")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -166,9 +149,6 @@ public class UserControllerTest {
 
     @Test
     void testLoginFailureUserNotFound() throws Exception {
-        Mockito.when(authenticationManager.authenticate(Mockito.any()))
-              .thenThrow(new BadCredentialsException("User not found"));
-
         mockMvc.perform(post("/api/users/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
